d6b99669bfdad46cac2ca1b8e46604e68778a163
CodeWothArjun <dolkhaliarjun9800@gmail.com>
2023-08-26 16:41:08 +0545

    added some program

--- 2nd Sem C++ Project/Qna8.cpp
+++ 2nd Sem C++ Project/Qna8.cpp
@@ -1,12 +1,44 @@
 // 8.	Write a program to add two complex numbers by overloading binary operator ( + ).
 #include <iostream>
 using namespace std;
-class ComplexOverLoad
+class ComOvLd
 {
+    float real, image;
+
 public:
+    ComOvLd(){};
+    ComOvLd(float rel, float img)
+    {
+        real = rel;
+        image = img;
+    }
+
+    ComOvLd
+    operator+(ComOvLd C)
+    {
+        ComOvLd add;
+        add.real = real + C.real;
+        add.image = image + C.image;
+        return add;
+    }
+    void display()
+    {
+        cout << real << " + " << image << "i";
+    }
 };
 
 int main()
 {
+    ComOvLd c1, c2, c3;
+    c1 = ComOvLd(2.3, 4.5);
+    c2 = ComOvLd(2.3, 4.5);
+    c3 = c1 + c2;
+    c1.display();
+    cout << endl;
+    c2.display();
+    cout << endl;
+    c3.display();
+    cout << endl;
+
     return 0;
 }
